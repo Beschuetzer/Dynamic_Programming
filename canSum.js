f02696a914862c1839ccc26182e6f67d08cd6e2f
Adam <adam.j.major@gmail.com>
2020-12-17 19:12:46 -0800

    Add howSum2

--- canSum.js
+++ canSum.js
@@ -1,13 +1,33 @@
 //write a fn canSum(targetSum, numberArray) that takes in a targetSum and an array of numbers and returns a boolean indicating whether or not it is possible to create the targetSum using any combination of the number any number of times.  All inputs are assumed to be non-negative 
+function canSum(targetSum, numbers) {
+    if (targetSum === 0) return true;
+    if (targetSum < 0) return false;
 
-function canSum(targetSum, numbers, memo={}) {
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        const nextTargetResult = canSum(remainder, numbers);
+        if (nextTargetResult === true) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(canSum(7, [2,3]))  //true;
+console.log(canSum(7, [5,3,4,7]))  //true;
+console.log(canSum(7, [2,4]))  //false;
+console.log(canSum(8, [2,3,5]))  //true;
+// console.log(canSum(300, [7,14]))  //false;
+
+
+function canSumMemo(targetSum, numbers, memo={}) {
     if (targetSum in memo) return memo[targetSum];
     if (targetSum === 0) return true;
     if (targetSum < 0) return false;
 
     for (let num of numbers) {
         const remainder = targetSum - num;
-        if (canSum(remainder, numbers, memo) === true) {
+        if (canSumMemo(remainder, numbers, memo) === true) {
             memo[remainder] = true;
             return true;
         }
@@ -16,9 +36,13 @@ function canSum(targetSum, numbers, memo={}) {
     return false;
 }
 
-console.log(canSum(7, [2,3]))  //true;
-console.log(canSum(7, [5,3,4,7]))  //true;
-console.log(canSum(7, [2,4]))  //false;
-console.log(canSum(8, [2,3,5]))  //true;
-console.log(canSum(300, [7,14]))  //false;
+console.log(canSumMemo(7, [2,3]))  //true;
+console.log(canSumMemo(7, [5,3,4,7]))  //true;
+console.log(canSumMemo(7, [2,4]))  //false;
+console.log(canSumMemo(8, [2,3,5]))  //true;
+console.log(canSumMemo(300, [7,14]))  //false;
+
+for (let num in {1: 'one', 2: 'two'}) {
+    console.log(num);
+}
 
--- howSum.js
+++ howSum.js
@@ -1,5 +1,31 @@
 //returns an array that contains a combination of numbers from numbers that add up to the targetSum, ff it exists otherwise return null.  May return any combination (not the smallest or longest one)
 
+function howSum2(targetSum, numbers, memo={}) {
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+    if (targetSum in memo) return memo[targetSum];
+
+    for (let num of numbers) {
+        const nextTargetSum = targetSum - num;
+        const nextTargetSumResult = howSum2(nextTargetSum, numbers, memo);
+        if (nextTargetSumResult !== null) {
+            nextTargetSumResult.push(num);
+            memo[nextTargetSum] = nextTargetSumResult;
+            return memo[nextTargetSum];
+        }
+    }
+    memo[targetSum] = null;
+    return memo[targetSum];
+}
+
+// console.log(howSum2(7, [2,3]))  //[3,2,2]
+// console.log(howSum2(7, [5,3,4,7]))  //[4,3];
+// console.log(howSum2(7, [2,4]))  //null;
+// console.log(howSum2(8, [2,3,5]))  //[2,2,2,2]];
+// console.log(howSum2(3000, [17,13]))  //[2,2,2,2]];
+console.log(howSum2(38980, [9,7,14,22,23,25]))  //maximum;
+
+
 function howSum(targetSum, numbers, memo={}) {
     if (targetSum in memo) return memo[targetSum];
     if (targetSum === 0) return [];
@@ -30,8 +56,9 @@ function howSum(targetSum, numbers, memo={}) {
 //time: O(n*m^2)
 //space: O(m^2)
 
-console.log(howSum(7, [2,3]))  //[3,2,2]
-console.log(howSum(7, [5,3,4,7]))  //[4,3];
-console.log(howSum(7, [2,4]))  //null;
-console.log(howSum(8, [2,3,5]))  //[2,2,2,2]];
-console.log(howSum(38975, [9,7,14,22,23,25]))  //maximum;
+// console.log(howSum(7, [2,3]))  //[3,2,2]
+// console.log(howSum(7, [5,3,4,7]))  //[4,3];
+// console.log(howSum(7, [2,4]))  //null;
+// console.log(howSum(8, [2,3,5]))  //[2,2,2,2]];
+// console.log(howSum(3000, [17,13]))  //[2,2,2,2]];
+console.log(howSum(38979, [9,7,14,22,23,25]))  //maximum;
